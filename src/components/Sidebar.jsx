df1eef3ff43376e2d5b980ebb3c4ddfc85e936a9
Hiren Shah <hiren@fancall.in>
2024-10-01 14:43:40 +0530

    my project

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -69,7 +69,7 @@ const Sidebar = () => {
         </Link>
 
         <div
-          className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white cursor-pointer"
+          className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white"
           onClick={() => dispatch(logOut())}
         >
           <CiLogout size={28} />
